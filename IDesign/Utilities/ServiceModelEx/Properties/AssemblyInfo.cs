f1bb766e95c440c49ddbf250acbb7dd6819d251f
Jörg Lang <jlang@evelix.ch>
2015-05-22 13:46:01 +0200

    Removed ServiceModelEx due to copyright

---
+++ IDesign/Utilities/ServiceModelEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿// © 2011 IDesign Inc. All rights reserved 
+//Questions? Comments? go to 
+//http://www.idesign.net
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyTitle("ServiceModelEx")]
+[assembly: AssemblyCompany("IDesign Inc.")]
+[assembly: AssemblyProduct("ServiceModelEx")]
+[assembly: AssemblyCopyright("Copyright © IDesign Inc. 2011")]
+[assembly: AssemblyVersion("3.0.0.0")]
